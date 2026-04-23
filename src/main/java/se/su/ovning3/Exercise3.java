08067c54bc5f932cff5284dbd758f9cb2130a431
Nils-Den <nils.d@live.se>
2026-04-23 09:34:02 +0200

    Filerna på plats

--- src/main/java/se/su/ovning3/Exercise3.java
+++ src/main/java/se/su/ovning3/Exercise3.java
@@ -1,10 +1,16 @@
 package se.su.ovning3;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class Exercise3 {
 
@@ -15,6 +21,22 @@ public class Exercise3 {
 	}
 
 	public void importRecordings(String fileName) {
+		try {
+			FileReader fileReader = new FileReader(fileName);
+			BufferedReader reader = new BufferedReader(fileReader);
+			String line;
+			line = reader.readLine();
+			// int noOfRecordings = Integer.parseInt(line);
+			while ((line = reader.readLine()) != null) {
+				recordings.add(parseLine(line, reader));
+			}
+			fileReader.close();
+			reader.close();
+		} catch (FileNotFoundException e) {
+			System.out.printf("%s not found%n", fileName);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
 	}
 
@@ -30,5 +52,35 @@ public class Exercise3 {
 		this.recordings.clear();
 		this.recordings.addAll(recordings);
 	}
-}
 
+	public Recording parseLine(String line, BufferedReader reader) {
+		String[] split = line.split(";");
+		String artist = split[0];
+		String title = split[1];
+		int year = Integer.parseInt(split[2]);
+		try{
+		String line2 = reader.readLine();
+		int noOfGenres = Integer.parseInt(line2);
+		Recording returnRecording = new Recording(title, artist, year, parseGenre(noOfGenres, reader));
+		return returnRecording;
+		}catch (IOException e){
+			System.out.printf("%s not found%n", "bajs");
+		}
+		return null;
+	}
+
+	public Set<String> parseGenre(int n, BufferedReader reader){
+		Set<String> genres = new HashSet<>();
+		for (int i = 0; i < n; i++){
+			try{
+				String line = reader.readLine();
+				genres.add(line);
+
+				
+			}catch(IOException e){
+				System.out.printf("%s not found%n", "bajs");
+			}
+		}
+		return genres;
+	}
+}
